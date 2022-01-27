cb332337aec63802b96c18a009f6078f716f23e8
Eugene <plakhotnikovvv@gmail.com>
2022-01-27 10:43:19 +0200

    change githuv url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_URL = 'https://newsapi.org/v2/top-headlines?country=us';
 export const API_KEY = '832a21276710497d8a5960e886aed0bd';
 
-export const USER_GITHUB_URL = 'https://github.com/eugenepla'
+export const USER_GITHUB_URL = 'https://github.com/eugenepla/test-task-news'
